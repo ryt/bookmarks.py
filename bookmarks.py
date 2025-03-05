aab68dbaa0aa03e70c01bad9b8e5d961e5be2967
Ray <ray@redment.com>
2025-03-05 01:41:31 -0800

    update: initial regex based parser start

--- bookmarks.py
+++ bookmarks.py
@@ -29,7 +29,57 @@ Usage:
 """
 
 import os
+import re
 import sys
+import json
+
+
+def parse_bookmarks(html):
+  bookmarks = []
+  stack = []
+  headers = []
+
+  html = re.sub(r'\n', '', html)
+  html = re.sub(r'\s+', ' ', html)
+  html = re.sub(r'>\s+<', '><', html)
+  html = re.sub(r'<([pP])>', r'<\1>\n', html)
+  html = re.sub(r'</(h1|H1|h3|H3|a|A)>', r'</\1>\n', html)
+
+  html = html.split('\n')
+
+  for line in html:
+
+    if line.startswith('<DL><p>'):
+      fold_name = headers[-1] if headers else ''
+      new_folder = {
+        'name'     : fold_name,
+        'type'     : 'folder',
+        'children' : []
+      }
+      if stack:
+        stack[-1]['children'].append(new_folder)
+      else:
+        bookmarks.append(new_folder)
+      stack.append(new_folder)
+
+    elif line.startswith('<DT><H3'):
+      title = re.sub(r'<DT><H3.*?>(.*?)</H3>', r'\1', line)
+      headers.append(title)
+
+    elif line.startswith('<DT><A'):
+      title = re.sub(r'<DT><A.*?>(.*?)</A>', r'\1', line)
+      new_child = {
+        'name' : title,
+        'type' : 'link',
+      }
+      if stack:
+        stack[-1]['children'].append(new_child)
+
+    elif line.startswith('</DL>'):
+      if stack:
+        stack.pop()
+
+  return bookmarks
 
 
 
@@ -51,8 +101,20 @@ def process_bookmarks_file(input, output='', third=''):
   </body>
   </html>'''
 
-  print(final_html)
 
+  with open(input, 'r', encoding='utf-8') as file:
+    html_content = file.read()
+  bookmarks = parse_bookmarks(html_content)
+  
+  out_json = json.dumps(bookmarks, indent=2)
+  out_file = re.sub(r'[\w_-]+\.html', 'bookmarks.py.html', input)
+
+  
+  with open(out_file, 'w', encoding='utf-8') as f:
+    json.dump(bookmarks, f, indent=2, ensure_ascii=False)
+
+  print(out_json)
+  print(f'Written to file {out_file}.')
 
 
 
